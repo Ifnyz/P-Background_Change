4a4fe3b30d9af9e66c3e061e6bc132528e6c0cbb
Ifnyz <119856741+Ifnyz@users.noreply.github.com>
2022-12-06 14:55:04 +0100

    Update background_loading.py

--- background_loading.py
+++ background_loading.py
@@ -6,7 +6,7 @@ import random
 class Main:
     def __init__(self):
         self.path = 'C:\\Users\\X495641\\Desktop'
-        for root, directories, files in os.walk(os.path.join(self.path, 'background')):
+        for root, directories, files in os.walk(os.path.join(self.path, 'background')): # i wish to make this line and the following simple but dont know how to do it ?
             self.background = [file.lower() for file in files if file.endswith(('.png', '.jpg', '.jpeg'))]
 
         ctypes.windll.user32.SystemParametersInfoW(20, 0, os.path.join(self.path, 'background', random.choice(self.background)) , 0)
